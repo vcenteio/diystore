2a126864ff20a9bbc9622336d1fe1e134c411293
Vitor Centeio <vitor.centeio@gmail.com>
2022-06-10 20:51:06 +0100

    feat(application): implement ProductOutputDTO class

--- diystore/application/get_products_use_case.py
+++ diystore/application/get_products_use_case.py
@@ -44,3 +44,29 @@ class GetProductsInputDTO(BaseModel):
     def _validate_rating_min_max(cls, rating):
         return round_decimal(rating, "1.0")
 
+
+class ProductOutputDTO(BaseModel):
+    id: str
+    ean: str
+    name: str
+    description: str
+    price: str
+    price_without_discount: str
+    discount: str
+    vat: str
+    in_stock: constr(to_lower=True)
+    height: str
+    width: str
+    length: str
+    color: str
+    material: str
+    country_of_origin: str
+    warranty: conint(strict=True)
+    category_id: str
+    category_name: str
+    client_rating: str
+    thumbnail_photo_url: str
+    medium_size_photo_url: str
+    large_size_photo_url: str
+    vendor_id: str
+    vendor_name: str
