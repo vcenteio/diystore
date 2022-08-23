6c9614615136afde6bce6cad49915af63fa04523
Vitor Centeio <vitor.centeio@gmail.com>
2022-08-23 15:19:35 +0100

    feat(api): implement /vendors endpoint

--- app/diystore/api/flaskrestapi/blueprints/vendor_bp.py
+++ app/diystore/api/flaskrestapi/blueprints/vendor_bp.py
@@ -1,11 +1,7 @@
 from flask import Blueprint
-from flask import request
 from flask import escape
-from flask import g
 
 from ..helpers import request_controller
-from ....infrastructure.controllers.web.exceptions import BadRequest
-from ....infrastructure.controllers.web.exceptions import ParameterMissing
 from ....infrastructure.controllers.web.product import ProductController
 
 
@@ -16,3 +12,9 @@ bp = Blueprint("vendor", __name__)
 @request_controller
 def get_vendor(vendor_id: str, controller: ProductController):
     return controller.get_vendor(vendor_id=escape(vendor_id))
+
+
+@bp.get("/vendors")
+@request_controller
+def get_vendors(controller: ProductController):
+    return controller.get_vendors()
